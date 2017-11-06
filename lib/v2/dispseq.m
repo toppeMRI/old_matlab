757838496101616ea565b1dd90644f784b38d8e5
jfnielse <jfnielse@brooks.engin.umich.edu>
2017-11-06 17:46:03 -0500

    modified:   lib/v2/dispseq.m

--- lib/v2/dispseq.m
+++ lib/v2/dispseq.m
@@ -146,7 +146,9 @@ end
 if dodisplay
 	T = (0:(numel(rho)-1))*dt/1000; % msec
 	gmax = 5;  % Gauss/cm
-	subplot(511); plot(T, rho); ylabel('rho');   axis([T(1) 1.01*T(end) -1.1*max(abs(rho)) 1.1*max(rho)]);
+        size(rho)
+    srho = max(1.1*max(abs(rho(:))),0.05);
+	subplot(511); plot(T, rho); ylabel('rho');   axis([T(1) 1.01*T(end) -srho srho]);
 	subplot(512); plot(T, th);  ylabel('theta'); axis([T(1) 1.01*T(end) -1.3*pi 1.3*pi]);
 	subplot(513); plot(T, gx);  ylabel('gx'); axis([T(1) 1.01*T(end) -1.05*gmax 1.05*gmax]);;
 	%gmax = 1;  % Gauss/cm
