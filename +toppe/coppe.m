86a54f40af788ea809895f21fdb3f506722909db
Amos Cao <amoscao@umich.edu>
2018-11-17 21:35:41 -0500

    Suppressed terminal output

--- +toppe/coppe.m
+++ +toppe/coppe.m
@@ -1,9 +1,9 @@
 % Packages toppe files into toppe-scanfiles.tgz, then copies it to the scanner
 % Assumes you have SSH keys set up to log into romero
-disp('Making archive...');
-system('tar czf toppe-scanfiles.tgz modules.txt scanloop.txt *.mod');
-disp('Copying to romero...');
-system('scp toppe-scanfiles.tgz fmrilab@romero:~/amos/');
-disp('Copying to scanner...');
-system('ssh -q fmrilab@romero "~/amos/pushtoppefiles"');
-disp('Done!');
+fprintf('Making archive...');
+[~,~] = system('tar czf toppe-scanfiles.tgz modules.txt scanloop.txt *.mod'); fprintf('done!\n');
+fprintf('Copying to romero...');
+[~,~] = system('scp toppe-scanfiles.tgz fmrilab@romero:~/amos/'); fprintf('done!\n');
+fprintf('Copying to scanner...');
+[~,~] = system('ssh -q fmrilab@romero "~/amos/pushtoppefiles"'); fprintf('done!\n');
+disp('Ready to scan.');
